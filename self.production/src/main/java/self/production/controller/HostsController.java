06399f517ccd0c7850867e1005713e3438a2aaa3
lwl8851206 <359392231@qq.com>
2014-09-20 12:30:14 +0800

    delete import-centence

--- self.production/src/main/java/self/production/controller/HostsController.java
+++ self.production/src/main/java/self/production/controller/HostsController.java
@@ -8,7 +8,6 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.ModelMap;
